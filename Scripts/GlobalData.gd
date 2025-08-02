d79580a97c1648549e253b0a5cc11df241ee5a99
Alex <alexander.james.swanson@gmail.com>
2025-08-02 02:32:44 -0400

    added Book types (needs to be UI-ed)

--- Scripts/GlobalData.gd
+++ Scripts/GlobalData.gd
@@ -1,18 +1,20 @@
 extends Node
 
 var loopNum: int = 0
+var isPlayerDead: bool = false
+
 var jumpLimit: int = 0
 var dashTime: float = 0
 var freezeTime: float = 0
-var isPlayerDead: bool = false
 
 func SaveData():
 	var file = FileAccess.open("GameData", FileAccess.WRITE)
 	file.store_var(loopNum)
+	file.store_var(isPlayerDead)
+	
 	file.store_var(jumpLimit)
 	file.store_var(dashTime)
 	file.store_var(freezeTime)
-	file.store_var(isPlayerDead)
 	file.close()
 	pass
 
@@ -20,10 +22,11 @@ func LoadData():
 	if FileAccess.file_exists("GameData"):
 		var file = FileAccess.open("GameData", FileAccess.READ)
 		loopNum = file.get_var()
+		isPlayerDead = file.get_var()
+		
 		jumpLimit = file.get_var()
 		dashTime = file.get_var()
 		freezeTime = file.get_var()
-		isPlayerDead = file.get_var()
 		file.close()
 	else:
 		SaveData()
